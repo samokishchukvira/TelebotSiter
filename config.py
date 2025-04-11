a621afce2b2fe416419c477b3545a22758c83d3c
samokishchukvira <159585110+samokishchukvira@users.noreply.github.com>
2025-04-11 14:54:25 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-TOKEN = "7870850985:AAGj1me_f1R208HswAvO85J0dYVUppuO4-M"
-DB_NAME = "telegram_bot"
-DB_USER = "postgres"
-DB_PASSWORD = "siterbot"
-DB_HOST = "localhost"
-DB_PORT = "5433"
-ADMIN_ID = 733841797
