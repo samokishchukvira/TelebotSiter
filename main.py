55c4898523252847c6122d14039a88ecbe315058
samokishchukvira <159585110+samokishchukvira@users.noreply.github.com>
2025-10-08 18:25:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -193,13 +193,12 @@ def complaint_text(message):
     if message.text in MENU_BUTTONS:
         if message.text == "Обрати іншу адресу 🔙":
             send_complex_menu(message.chat.id)
+            return
+        if message.text == "Наші контакти":
+            contacts(message)
+            return
+        bot.send_message(message.chat.id, "⚠️ Спочатку введіть свої дані 🏠")
         return
-    if message.text == "Наші контакти":
-        contacts(message)
-        return
-
-    bot.send_message(message.chat.id, "⚠️ Спочатку введіть інформацію 🏠")
-    return
         
     user_data[message.chat.id]["waiting_complaint"] = False
     text = message.text
@@ -347,3 +346,4 @@ if __name__ == "__main__":
         except Exception as e:
             print(f"Polling помилка: {e}")
             time.sleep(5)
+
