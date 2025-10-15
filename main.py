44baf131fa05f71e8f49591d1b2876f61d3b55d2
samokishchukvira <159585110+samokishchukvira@users.noreply.github.com>
2025-10-15 15:09:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telebot
 from telebot import types
 
 TOKEN = "8297975325:AAGArbLcJE9dSsMxoZmIzxx41wjBbKwg54I"
-ADMIN_IDS = [771177410, 394592253, 1336042507, 6334390799, 527797499, 733841797]
+ADMIN_IDS = [733841797, 394592253, 6334390799, 771177410, 1336042507, 527797499]
 # , 394592253, 1336042507, 6334390799, 527797499, 771177410
 # 6334390799, 1336042507, 527797499, 771177410, 394592253, 733841797
 bot = telebot.TeleBot(TOKEN)
@@ -406,6 +406,7 @@ if __name__ == "__main__":
         except Exception as e:
             print(f"Polling помилка: {e}")
             time.sleep(5)
+
 
 
 
